099a874f27c7e0ab69296189271cddcad8ab5651
Abdellah-Sk <70332210+Abdellah-Sk@users.noreply.github.com>
2025-10-20 15:40:03 +0200

    Initial Commit

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Abdellah Skoundri on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
