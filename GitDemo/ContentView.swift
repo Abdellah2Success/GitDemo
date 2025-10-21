fd7f9a0976618ad6d99da49c0c9c286daf2ba0c4
Abdellah-Sk <70332210+Abdellah-Sk@users.noreply.github.com>
2025-10-21 16:18:51 +0200

    add icon

--- GitDemo/ContentView.swift
+++ GitDemo/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            HStack {
+                Image(systemName: "globe")
+                Image(systemName: "person.fill")
+            }
+            .imageScale(.large)
+            .foregroundStyle(.tint)
+            
             Text("Hello, world!")
         }
         .padding()
